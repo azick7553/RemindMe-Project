e811a286caaa83827beb355fa33a4cd34f95db98
Azick <Azick@AZICK-PC>
2018-08-16 18:12:57 +0500

    Change API to 23, added toolbar and changed icon, added colors.

--- app/src/main/java/com/remind/azick/remindme/MainActivity.java
+++ app/src/main/java/com/remind/azick/remindme/MainActivity.java
@@ -3,12 +3,28 @@ package com.remind.azick.remindme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MainActivity extends Activity{
 
+    private android.support.v7.widget.Toolbar toolbar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }

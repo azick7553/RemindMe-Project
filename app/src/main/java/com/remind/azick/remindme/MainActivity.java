7ad6b5c8d42f05d9f5a9358fdfa8ac89fd3fda77
Azick <Azick@AZICK-PC>
2018-08-16 19:16:45 +0500

    added navBar and moved toolbar to main_activity.xml and removed toolbar.xml

--- app/src/main/java/com/remind/azick/remindme/MainActivity.java
+++ app/src/main/java/com/remind/azick/remindme/MainActivity.java
@@ -1,25 +1,26 @@
 package com.remind.azick.remindme;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.Toolbar;
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity{
 
-    private android.support.v7.widget.Toolbar toolbar;
+    private Toolbar toolbar;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolbar();
     }
 
     private void initToolbar() {
-        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
-        toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
                 return false;

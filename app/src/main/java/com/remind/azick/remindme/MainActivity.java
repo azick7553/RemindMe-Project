a033ee55040b0e9752df31dfcdf2542c5d62f93a
Azick <Azick@AZICK-PC>
2018-08-19 01:13:30 +0500

    added abstract class fragment for tabs

--- app/src/main/java/com/remind/azick/remindme/MainActivity.java
+++ app/src/main/java/com/remind/azick/remindme/MainActivity.java
@@ -1,7 +1,6 @@
 package com.remind.azick.remindme;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -11,7 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-import com.remind.azick.remindme.adapter.TabsPagerFragmentAdapter;
+import com.remind.azick.remindme.adapter.TabsFragmentAdapter;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -48,7 +47,7 @@ public class MainActivity extends AppCompatActivity{
 
     private void initTabs() {
         viewPager = (ViewPager)findViewById(R.id.viewPager);
-        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        TabsFragmentAdapter adapter = new TabsFragmentAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(adapter);
 
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
--- app/src/main/java/com/remind/azick/remindme/fragment/BirthdaysFragment.java
+++ app/src/main/java/com/remind/azick/remindme/fragment/BirthdaysFragment.java
@@ -1,25 +1,24 @@
 package com.remind.azick.remindme.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.remind.azick.remindme.R;
 
-public class ExampleFragment extends Fragment {
+public class BirthdaysFragment extends AbstractTabFragment {
 
     private static final  int LAYOUT = R.layout.fragment_example;
 
-    private View view;
-
-    public static ExampleFragment getInstance(){
+    public static BirthdaysFragment getInstance(Context context){
         Bundle args = new Bundle();
-        ExampleFragment fragment = new ExampleFragment();
+        BirthdaysFragment fragment = new BirthdaysFragment();
         fragment.setArguments(args);
+        fragment.setContext(context);
+        fragment.setTitle(context.getString(R.string.tab_item_birthday));
 
         return fragment;
     }
@@ -30,4 +29,9 @@ public class ExampleFragment extends Fragment {
         view = inflater.inflate(LAYOUT,container,false);
         return view;
     }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
 }
